81ae0f16f77892e8bf1719a63013119c579c6ece
priyadharshini231298 <priyadharshini231298@gmail.com>
2019-05-01 03:32:31 +0530

    Add files via upload

--- modified.py
+++ modified.py
@@ -8,15 +8,12 @@ for para in doc.paragraphs:
 	print(para.text)
 	blob=TextBlob(para.text)
 	for words,tag in blob.tags:
-                       # if tag=='JJ' or tag=='NNP':
 		lst.append(words)
-		#print(lst)
-print("Document Name:",lst[0],lst[1],lst[2])
-for i in range(0,10):
+
+print("Document Name : ",end="")
+for i in range(0,len(lst)):
 	blob1=TextBlob(lst[i])
 	for wrd,tag1 in blob1.tags:
-		#print(wrd)
-		#print(tag1)
 		if tag1=='NN' or tag1=='NNP':
 			print(lst[i],end=" ")
 		else:
